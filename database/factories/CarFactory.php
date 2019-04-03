71d462d503725a89958327cf7a2c93c12eaa50ec
ayushi <am2862@njit.edu>
2019-04-03 17:45:13 -0400

    Commit5 Create factory and assign make field

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Car::class, function (Faker $faker) {
+    return [
+        'Model' => $faker->name,
+        'Year' => $faker->year,
+        'Make' => $faker->randomElement('honda', 'ford', 'toyota'),
+    ];
+});
