2e2a626b5072c3b93aa9c0855cb06023524918cd
ayushi <am2862@njit.edu>
2019-04-04 22:48:41 -0400

    Commit16 UnitTest8 Count number of user records

---
+++ tests/Unit/CountUserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CountUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCountUserTest()
+    {
+        $user= User::all();
+        $this->assertCount(50,$user);
+    }
+}
